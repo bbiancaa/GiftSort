a873895bfc889f25de41c395a891053d9f6ec75c
mascDriver <diogobaltazardonascimento@outlook.com>
2022-05-31 22:02:33 -0300

    adicionando algumas tabelas

--- raffle/admin.py
+++ raffle/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Participant, CategoryGift, Room)
+class ParticipantAdmin(admin.ModelAdmin):
+    pass
--- raffle/models.py
+++ raffle/models.py
@@ -2,7 +2,21 @@ from django.db import models
 import uuid
 
 
+class CategoryGift(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    category = models.ForeignKey(CategoryGift, on_delete=models.CASCADE)
+    obs = models.TextField()
+    host = models.BooleanField(default=False)
+
+
 class Room(models.Model):
-    id = models.UUIDField(default=uuid.uuid4)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
     name = models.CharField(max_length=200, verbose_name='Name of Room')
     link = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
+    participant = models.ManyToManyField(Participant)

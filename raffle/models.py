c8a56778105418c39a0306291498a771e6d5e866
leobsilva <leobsilva12@hotmail.com>
2022-06-07 21:40:46 -0300

    Modificado models do raffle

--- raffle/models.py
+++ raffle/models.py
@@ -1,22 +1,32 @@
+from operator import truediv
 from django.db import models
 import uuid
 
 
 class CategoryGift(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class Participant(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    category = models.ForeignKey(CategoryGift, on_delete=models.CASCADE)
-    obs = models.TextField()
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    email = models.CharField(max_length=100, null=False, unique=True)
+    category = models.ForeignKey(CategoryGift,  on_delete=models.CASCADE)
+    obs = models.TextField(blank=True, null=True, verbose_name='Observation')
     host = models.BooleanField(default=False)
-
+    
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
-    name = models.CharField(max_length=200, verbose_name='Name of Room')
-    link = models.CharField(max_length=200)
-    value = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name='Name of Room', null=False)
+    link = models.CharField(max_length=100)
+    min_value = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='Minimum value')
+    max_value = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='Maximum value')
     participant = models.ManyToManyField(Participant)
+
+    def __str__(self):
+        return self.name

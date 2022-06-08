2da4e1249c9551d7da386108b503dfbafe068324
bbiancaa <biancagabriela.fritsch@hotmail.com>
2022-06-07 21:27:34 -0300

    inserindo lading page do site

--- giftsort/settings.py
+++ giftsort/settings.py
@@ -116,8 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
+STATIC_ROOT = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join('assets'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
